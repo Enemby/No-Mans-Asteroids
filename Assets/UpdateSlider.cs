dcf00cefab64104c43b3ef74db3d160d57b19cee
Enemby <Enemby@users.noreply.github.com>
2024-04-11 02:02:52 -0600

    Update UpdateSlider.cs

--- Assets/UpdateSlider.cs
+++ Assets/UpdateSlider.cs
@@ -1,20 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 [System.Serializable]
 public partial class UpdateSlider : MonoBehaviour
 {
     public string myPref;
-    public UI.Slider mySlider;
+    public Slider mySlider;
     public virtual void Start()
     {
         if (this.mySlider == false)
         {
-            this.mySlider = (error) this.GetComponent("UI.Slider");
+            this.mySlider = this.GetComponent<Slider>();
         }
         if (PlayerPrefs.HasKey(this.myPref))
         {
-            this.mySlider.value = (error) PlayerPrefs.GetFloat(this.myPref);
+            this.mySlider.value = PlayerPrefs.GetFloat(this.myPref);
         }
     }
 
