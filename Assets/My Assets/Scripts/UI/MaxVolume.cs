ad06a2b9f2a14e6562ccb09a8b23fa89ab7f052f
Enemby <Enemby@users.noreply.github.com>
2024-04-11 13:01:13 -0600

    Update MaxVolume.cs

--- Assets/My Assets/Scripts/UI/MaxVolume.cs
+++ Assets/My Assets/Scripts/UI/MaxVolume.cs
@@ -7,7 +7,7 @@ public partial class MaxVolume : MonoBehaviour
     public virtual void Update()
     {
         float maxVolume = PlayerPrefs.GetFloat("maxVolume");
-        this.GetComponent("AudioListener").volume = maxVolume;
+        AudioListener.volume = maxVolume;
     }
 
 }
